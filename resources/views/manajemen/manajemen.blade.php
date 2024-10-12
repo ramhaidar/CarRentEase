2b6f22d7983d795f228c5b7820091e2bf27e7edd
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2024-10-12 21:35:10 +0700

    Install jQuery untuk Modal Hapus Data

--- resources/views/manajemen/manajemen.blade.php
+++ resources/views/manajemen/manajemen.blade.php
@@ -69,7 +69,7 @@
                                             </a>
 
                                             <!-- Tombol Hapus -->
-                                            <button class="text-red-600 hover:text-red-900" onclick="openModal('{{ $mobil->id }}')">
+                                            <button class="text-red-600 hover:text-red-900 open-modal" data-id="{{ $mobil->id }}">
                                                 <i class="fas fa-trash-alt"></i>
                                             </button>
                                         </td>
@@ -116,7 +116,7 @@
                             {{ __('Hapus') }}
                         </button>
                     </form>
-                    <button class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" type="button" onclick="closeModal()">
+                    <button class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" id="close-modal" type="button">
                         {{ __('Batal') }}
                     </button>
                 </div>
@@ -124,14 +124,19 @@
         </div>
     </div>
 
-    <script>
-        function openModal(mobilId) {
-            document.getElementById('modal').classList.remove('hidden');
-            document.getElementById('delete-form').action = '/manajemen/' + mobilId;
-        }
+    <script type="module">
+        $(document).ready(function() {
+            // Buka modal dengan jQuery
+            $('.open-modal').click(function() {
+                var mobilId = $(this).data('id');
+                $('#delete-form').attr('action', '/manajemen/' + mobilId);
+                $('#modal').removeClass('hidden');
+            });
 
-        function closeModal() {
-            document.getElementById('modal').classList.add('hidden');
-        }
+            // Tutup modal dengan jQuery
+            $('#close-modal').click(function() {
+                $('#modal').addClass('hidden');
+            });
+        });
     </script>
 </x-app-layout>
