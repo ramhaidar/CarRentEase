e195267393f040eadaed285c4255bbd4e75201ba
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2024-10-12 10:38:27 +0700

    Change Locale to Indonesia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,7 +10,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register () : void
     {
         //
     }
@@ -17,8 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot () : void
     {
         //
+        config ( [ 'app.locale' => 'id' ] );
+        Carbon::setLocale ( 'id' );
+        date_default_timezone_set ( 'Asia/Jakarta' );
     }
 }
